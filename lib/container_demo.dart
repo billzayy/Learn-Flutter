db553b3bfd4c6f433996065030081268db2dc09f
billzayy <coderbillzay@gmail.com>
2023-05-10 23:14:17 +0700

    New .gitignore

--- lib/container_demo.dart
+++ lib/container_demo.dart
@@ -10,32 +10,30 @@ class ContainerDemo extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Startup Name Generator'),
-            backgroundColor: Colors.redAccent,
-          ),
-          body: Container(
-            width: 1000,
-            height: 100,
-            // color: Colors.greenAccent,
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.greenAccent,
-            ),
-            transform: Matrix4.rotationZ(pi / 8),
-            child: const Text("Hello World!",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                )
-              ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.pop(context),
-            child: Icon(Icons.arrow_back)
-          ),
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          backgroundColor: Colors.redAccent,
         ),
+        body: Container(
+          width: 1000,
+          height: 100,
+          // color: Colors.greenAccent,
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.greenAccent,
+          ),
+          transform: Matrix4.rotationZ(pi / 10),
+          child: const Text("Hello World!",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              )),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Icon(Icons.arrow_back)),
+      ),
     );
   }
 }
